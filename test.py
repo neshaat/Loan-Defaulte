c1f088e74479e32438cb5d8c419d0cbd06831714
neshatgohari <neshaatgohari93@gmail.com>
2024-08-22 03:19:20 -0400

    debuging the test

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 from pyspark.sql import SparkSession
 from pyspark.ml.tuning import CrossValidatorModel
 from pyspark.ml.feature import VectorAssembler, StringIndexer
-from pyspark.ml import PipelineModel
 
 
 def load_model():
@@ -10,31 +9,42 @@ def load_model():
         .appName("LoanDefaulterPrediction") \
         .getOrCreate()
 
-    # Load trained model
-    loaded_model = PipelineModel.load("./model")
-    return spark, loaded_model
+    model = CrossValidatorModel.load("./model")
 
-def predict_loan_default(spark, loaded_model, test_data):
-    # Create a DataFrame from test data
-    synthetic_test_data = spark.createDataFrame([test_data], 
-        ["AMT_INCOME_TOTAL", "AMT_CREDIT", "AMT_ANNUITY", "DAYS_EMPLOYED", "DAYS_REGISTRATION", "FLAG_OWN_CAR", "FLAG_OWN_REALTY"])
+    return spark, model
 
-    # Preprocess test data
-    indexers = [StringIndexer(inputCol=column, outputCol=column+"_index").fit(synthetic_test_data) for column in ['FLAG_OWN_CAR', 'FLAG_OWN_REALTY']]
-    indexed_test_data = synthetic_test_data
+
+def preprocess_data(spark):
+    synthetic_test_data = spark.createDataFrame([
+        (202500.0, 406597.5, 24700.5, -637, -3648.0, "Y", "Y"),
+        (270000.0, 1293502.5, 35698.5, -1188, -1186.0, "N", "N"),
+        (90500.0, 135000.0, 6750.0, -225, -4260.0, "Y", "Y")
+    ], ["AMT_INCOME_TOTAL", "AMT_CREDIT", "AMT_ANNUITY", "DAYS_EMPLOYED", "DAYS_REGISTRATION", "FLAG_OWN_CAR",
+        "FLAG_OWN_REALTY"])
+
+    indexers = [
+        StringIndexer(inputCol="FLAG_OWN_CAR", outputCol="FLAG_OWN_CAR_index"),
+        StringIndexer(inputCol="FLAG_OWN_REALTY", outputCol="FLAG_OWN_REALTY_index")
+    ]
+
     for indexer in indexers:
-        indexed_test_data = indexer.transform(synthetic_test_data)
+        synthetic_test_data = indexer.fit(synthetic_test_data).transform(synthetic_test_data)
 
-    # Create feature vector
-    feature_cols = ['AMT_INCOME_TOTAL', 'AMT_CREDIT', 'AMT_ANNUITY', 'DAYS_EMPLOYED', 'DAYS_REGISTRATION', 'FLAG_OWN_CAR_index', 'FLAG_OWN_REALTY_index']
+    feature_cols = ['AMT_INCOME_TOTAL', 'AMT_CREDIT', 'AMT_ANNUITY', 'DAYS_EMPLOYED',
+                    'DAYS_REGISTRATION', 'FLAG_OWN_CAR_index', 'FLAG_OWN_REALTY_index']
     assembler = VectorAssembler(inputCols=feature_cols, outputCol='features')
-    data_with_features_test = assembler.transform(indexed_test_data)
+    data_with_features_test = assembler.transform(synthetic_test_data)
 
-    # Make predictions on test data
-    predictions_test = loaded_model.transform(data_with_features_test)
-    result = predictions_test.select("prediction", "probability").collect()[0]
-    
-    # Stop SparkSession
-    spark.stop()
+    return data_with_features_test
 
-    return {"prediction": result.prediction, "probability": result.probability}
+
+def predict_loan_default(spark, model):
+    data_with_features_test = preprocess_data(spark)
+    predictions_test = model.transform(data_with_features_test)
+    predictions_test.select("prediction").show()
+
+
+if __name__ == "__main__":
+    spark, model = load_model()
+
+    predict_loan_default(spark, model)
